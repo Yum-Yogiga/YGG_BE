e7c50822ed03ad2a738394a7989139009b54fa62
taehoon <xogns1514@gmail.com>
2023-05-13 17:16:32 +0900

    Initial commit

---
+++ src/main/java/com/yogiga/yogiga/YogigaApplication.java
@@ -0,0 +1,13 @@
+package com.yogiga.yogiga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogigaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogigaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yogiga/yogiga/YogigaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yogiga.yogiga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YogigaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
