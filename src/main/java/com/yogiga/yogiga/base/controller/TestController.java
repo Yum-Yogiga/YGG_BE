594acf21857399b9544537ef1a2768b6b8ae8790
taehoon <xogns1514@gmail.com>
2023-05-13 17:31:21 +0900

    환경test

---
+++ src/main/java/com/yogiga/yogiga/base/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.yogiga.yogiga.base.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    String hello(){
+        return "hello";
+    }
+}
