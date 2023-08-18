010484f9f404eba861d0d8f5b9ec79d80c76afde
taehoon <xogns1514@gmail.com>
2023-08-18 19:39:42 +0900

    feat: swagger 설명 추가

--- src/main/java/com/yogiga/yogiga/global/controller/EmailController.java
+++ src/main/java/com/yogiga/yogiga/global/controller/EmailController.java
@@ -1,6 +1,7 @@
 package com.yogiga.yogiga.global.controller;
 
 import com.yogiga.yogiga.global.service.EmailService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class EmailController {
     private final EmailService emailService;
-
+    @Operation(summary = "email 인증 api, email 값넘겨주면, 해당 email로 전송된 인증번호 반환됨. ")
     @GetMapping("email-verification")
     public ResponseEntity<String> emailVerify(@RequestParam String email) throws Exception{
         return ResponseEntity.ok(emailService.sendEmailAuthenticationCode(email));
