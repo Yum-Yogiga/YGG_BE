a7c119e0080b0c164aec1235fa972154ff8f3154
taehoon <xogns1514@gmail.com>
2023-08-18 19:24:47 +0900

    feat: logger 추가

--- src/main/java/com/yogiga/yogiga/user/util/SecurityUtil.java
+++ src/main/java/com/yogiga/yogiga/user/util/SecurityUtil.java
@@ -4,8 +4,9 @@ import com.yogiga.yogiga.global.exception.CustomException;
 import com.yogiga.yogiga.global.exception.ErrorCode;
 import com.yogiga.yogiga.user.entity.User;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
-
+@Slf4j
 @NoArgsConstructor
 public class SecurityUtil {
     public static User getUser() {
@@ -13,6 +14,7 @@ public class SecurityUtil {
         if (user == null) {
             throw new CustomException(ErrorCode.USER_NOT_FOUND_ERROR, "존재하지 않는 회원입니다. ");
         }
+        log.info("user={}", user);
         return user;
     }
 
