f523f48cc32dc748f65b42e716e819477178549e
taehoon <xogns1514@gmail.com>
2023-10-17 15:41:36 +0900

    feat: Review entity 생성

---
+++ src/main/java/com/yogiga/yogiga/review/entity/Review.java
@@ -0,0 +1,31 @@
+package com.yogiga.yogiga.review.entity;
+
+import com.yogiga.yogiga.global.entity.BaseTimeEntity;
+import com.yogiga.yogiga.restaurant.entity.Restaurant;
+import com.yogiga.yogiga.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    Restaurant restaurant;
+
+    @Column(nullable = false)
+    String content;
+
+}
