7ce734ca74638ee390ae964d4fda91fae4138b93
taehoon <xogns1514@gmail.com>
2023-11-23 10:57:48 +0900

    feat(RestaurantService): 키워드 선택 수 추가

--- src/main/java/com/yogiga/yogiga/keyword/repository/RestaurantKeywordRepository.java
+++ src/main/java/com/yogiga/yogiga/keyword/repository/RestaurantKeywordRepository.java
@@ -5,7 +5,11 @@ import com.yogiga.yogiga.keyword.entity.RestaurantKeyword;
 import com.yogiga.yogiga.restaurant.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RestaurantKeywordRepository extends JpaRepository<RestaurantKeyword, Long> {
 
     RestaurantKeyword findByRestaurantAndKeyword(Restaurant restaurant, Keyword keyword);
+
+    List<RestaurantKeyword> findByRestaurant(Restaurant restaurant);
 }
